fbb19555c40439daf6ba7902648b49443c4f7232
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2015-08-31 09:25:21 +0600

    Added various logic

--- js/activator.js
+++ js/activator.js
@@ -3,53 +3,60 @@
  */
 
 /*
-|--------------------------------------------------------------------------
-| Run the following function when the DOM is ready
-|--------------------------------------------------------------------------
-*/
+ |--------------------------------------------------------------------------
+ | Run the following function when the DOM is ready
+ |--------------------------------------------------------------------------
+ */
 jQuery(document).ready(function () {
 
     /*
-    |--------------------------------------------------------------------------
-    | Run the following function when the activator form is submit
-    |--------------------------------------------------------------------------
-    */
+     |--------------------------------------------------------------------------
+     | Run the following function when the activator form is submit
+     |--------------------------------------------------------------------------
+     */
     jQuery('#activator').on('submit', function (event) {
 
         /*
-        |--------------------------------------------------------------------------
-        | Disable default submit button behaviour
-        |--------------------------------------------------------------------------
-        */
+         |--------------------------------------------------------------------------
+         | Disable default submit button behaviour
+         |--------------------------------------------------------------------------
+         */
         event.preventDefault();
 
         /*
-        |--------------------------------------------------------------------------
-        | Start Ajax connection
-        |--------------------------------------------------------------------------
-        */
+         |--------------------------------------------------------------------------
+         | Start Ajax connection
+         |--------------------------------------------------------------------------
+         */
         jQuery.ajax({
 
             /*
-            |--------------------------------------------------------------------------
-            | API path
-            |--------------------------------------------------------------------------
-            */
+             |--------------------------------------------------------------------------
+             | Send the ajax request to functions.php file
+             |--------------------------------------------------------------------------
+             */
             url: '/wp-content/plugins/activator/functions.php',
 
             /*
-            |--------------------------------------------------------------------------
-            | Data sending method to the API: "body {x-www-form-urlencoded}"
-            |--------------------------------------------------------------------------
-            */
+             |--------------------------------------------------------------------------
+             | Data sending method to the API: "body {x-www-form-urlencoded}"
+             |--------------------------------------------------------------------------
+             */
             data: jQuery(this).serialize(),
 
             /*
-            |--------------------------------------------------------------------------
-            | API request method
-            |--------------------------------------------------------------------------
-            */
+             |--------------------------------------------------------------------------
+             | API request method
+             |--------------------------------------------------------------------------
+             */
             method: 'POST',
+
+            /*
+             |--------------------------------------------------------------------------
+             | Response data type
+             |--------------------------------------------------------------------------
+             */
+            dataType: 'json',
 
             /*
              |--------------------------------------------------------------------------
@@ -68,10 +75,59 @@ jQuery(document).ready(function () {
             complete: function () {
                 jQuery('#pre-loader').hide();
             },
+
             success: function (data) {
-                console.log(data);
+
+                /*
+                 |--------------------------------------------------------------------------
+                 | Already activated - Same computer ID
+                 |--------------------------------------------------------------------------
+                 */
+                if (data.Status == 'Already activated - Same Computer ID') {
+                    jQuery('.activation-code').show();
+                    jQuery('.activation-code .status').text('Already activated: Copy the below activation code and activate your copy of LT Auditor+');
+                    jQuery('.activation-code .activated-license-code pre').html(data.ActivatedLicenseCode);
+                }
+
+                /*
+                 |--------------------------------------------------------------------------
+                 | reCAPTCHA failure
+                 |--------------------------------------------------------------------------
+                 */
+                else if (data.Status == 'failed') {
+                    jQuery('.Google-reCAPTCHA .verify-recaptcha').show();
+                }
+
+                /*
+                 |--------------------------------------------------------------------------
+                 | Already activated - Different Computer ID
+                 |--------------------------------------------------------------------------
+                 */
+                else if ('Error : Already activated - Different Computer ID') {
+                    jQuery('.activation-code').show();
+                    jQuery('.activation-code .status').text('License cannot be activated!');
+                    jQuery('.activation-code .activated-license-code pre').html('<h3 class="different-computer-id">Already activated using different computer ID.</h3><h3 class="different-computer-id">Please contact Blue Lance to get another serial key.</h3>');
+                }
+
+                /*
+                 |--------------------------------------------------------------------------
+                 | Successful
+                 |--------------------------------------------------------------------------
+                 */
+                else {
+                    jQuery('.activation-code').show();
+                    jQuery('.activation-code .status').text('Copy the below activation code and activate your copy of LT Auditor+');
+                    jQuery('.activation-code .activated-license-code pre').html(data.ActivatedLicenseCode);
+                }
+            },
+
+            error: function () {
+                jQuery('.activation-code').show();
+                jQuery('.activation-code .status').text('Internal Error Activating License');
+                jQuery('.activation-code .activated-license-code pre').html('<h3 class="different-computer-id">Please contact Blue Lance to get another serial key.</h3>');
             }
         });
+
         return false;
     });
 });
